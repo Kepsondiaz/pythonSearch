261b118040616bdd71c526e16ee12226017e3d37
kepsonDiaz <kherabadiaby328@gmail.com>
2022-12-04 02:43:18 +0000

    lists.py

---
+++ lists.py
@@ -0,0 +1,52 @@
+# Créer une liste en python
+listesEleve = []
+
+# Ajouter une valeur à une liste python
+listesEleve = ['Mahamadou', 'Omar', 'Michiel']
+
+# Ou les ajouter après la création de la liste avec la méthode append (qui signifie "ajouter" en anglais):
+listesEleve.append('Fatima') 
+listesEleve.append(22) 
+listesEleve.append('Awa') 
+listesEleve.append('15') 
+print(listesEleve)
+
+# Afficher un item d'une liste
+
+listesEleve[3] = "Aminata"
+print(listesEleve[3]) # Aminata 
+
+# Supprimer une entrée avec un index
+del listesEleve[4] # ['Mahamadou', 'Omar', 'Michiel', 'Fatima', 'Awa', '15']
+del listesEleve[3] # ['Mahamadou', 'Omar', 'Michiel', 'Awa', '15']
+print(listesEleve)
+
+# Supprimer une entrée avec sa valeur
+
+listesEleve.remove("Mahamadou")
+listesEleve
+
+# Inverser les valeurs d'une liste
+listesEleve.reverse()
+print(listesEleve) # ['15', 'Awa', 'Michiel', 'Omar', 'Mahamadou']
+
+
+# Compter le nombre d'occurences d'une valeur
+listesEleve.append('Mahamadou') # ajout Mahamadou
+listesEleve.append('Mahamadou') # ajout Mahamadou
+listesEleve.append('Mahamadou') # ajout Mahamadou
+listesEleve.append('Mahamadou') # ajout Mahamadou
+
+countIndex = listesEleve.count('Mahamadou')
+
+print(countIndex)
+
+# Compter le nombre d'items d'une liste
+print(listesEleve.__len__())
+
+# Manipuler une liste
+listeNombre = [1, 10, 100, 250, 500]
+print(listeNombre[0]) # affiche 1
+print(listeNombre[-1]) # affiche 500
+print(listeNombre[-4]) # affiche 10
+print(listeNombre[:]) # Affiche toutes les occurences
