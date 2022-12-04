bb509c21187bca77b03f2751443be5669cb111ef
kepsonDiaz <kherabadiaby328@gmail.com>
2022-12-04 02:43:43 +0000

    lists.py

--- lists.py
+++ lists.py
@@ -1,37 +1,35 @@
-# Créer une liste en python
+## Créer une liste en python ##
 listesEleve = []
 
-# Ajouter une valeur à une liste python
+## Ajouter une valeur à une liste python ##
 listesEleve = ['Mahamadou', 'Omar', 'Michiel']
 
-# Ou les ajouter après la création de la liste avec la méthode append (qui signifie "ajouter" en anglais):
+## Ou les ajouter après la création de la liste avec la méthode append (qui signifie "ajouter" en anglais): ##
 listesEleve.append('Fatima') 
 listesEleve.append(22) 
 listesEleve.append('Awa') 
 listesEleve.append('15') 
-print(listesEleve)
+print(listesEleve)  # ['Mahamadou', 'Omar', 'Michiel', 'Fatima', 22, 'Awa', '15']
 
-# Afficher un item d'une liste
-
+## Afficher un item d'une liste ##
 listesEleve[3] = "Aminata"
 print(listesEleve[3]) # Aminata 
 
-# Supprimer une entrée avec un index
-del listesEleve[4] # ['Mahamadou', 'Omar', 'Michiel', 'Fatima', 'Awa', '15']
-del listesEleve[3] # ['Mahamadou', 'Omar', 'Michiel', 'Awa', '15']
-print(listesEleve)
+## Supprimer une entrée avec un index ##
+del listesEleve[4] 
+del listesEleve[3] 
+print(listesEleve)  # ['Mahamadou', 'Omar', 'Michiel', 'Awa', '15']
 
-# Supprimer une entrée avec sa valeur
-
+## Supprimer une entrée avec sa valeur ##
 listesEleve.remove("Mahamadou")
 listesEleve
 
-# Inverser les valeurs d'une liste
+## Inverser les valeurs d'une liste ##
 listesEleve.reverse()
 print(listesEleve) # ['15', 'Awa', 'Michiel', 'Omar', 'Mahamadou']
 
 
-# Compter le nombre d'occurences d'une valeur
+## Compter le nombre d'occurences d'une valeur ##
 listesEleve.append('Mahamadou') # ajout Mahamadou
 listesEleve.append('Mahamadou') # ajout Mahamadou
 listesEleve.append('Mahamadou') # ajout Mahamadou
@@ -39,10 +37,10 @@ listesEleve.append('Mahamadou') # ajout Mahamadou
 
 countIndex = listesEleve.count('Mahamadou')
 
-print(countIndex)
+print(countIndex) # 5
 
-# Compter le nombre d'items d'une liste
-print(listesEleve.__len__())
+## Compter le nombre d'items d'une liste ##
+print(listesEleve.__len__()) # 9
 
 # Manipuler une liste
 listeNombre = [1, 10, 100, 250, 500]
